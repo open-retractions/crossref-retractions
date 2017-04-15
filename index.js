3180d30bff5c34e4a24e8acf9b09a4a96b17e69a
Rik Smith-Unna <rik@fathomlabs.io>
2017-04-15 22:54:07 +0300

    fix progress

--- index.js
+++ index.js
@@ -21,10 +21,9 @@ var types = [
 module.exports = function () {
   var args = {
     filter: ['is-update:true'],
-    progress: true
   }
 
   types.forEach(function (type) { args.filter.push('update-type:' + type) })
 
-  return crossref(args)
+  return crossref(args, { progress: true })
 }
